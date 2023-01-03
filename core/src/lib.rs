74007322ce6f6f33bfef0e24b74e9e62d8955e27
paulopacitti <ppacitti@outlook.com>
2023-01-03 12:01:41 -0300

    Implement op code routines for skips and bitwise operations

--- core/src/lib.rs
+++ core/src/lib.rs
@@ -73,8 +73,7 @@ impl Emulator {
 
     pub fn tick(&mut self) {
         let op = self.fetch();
-        // TODO: Decode
-        // TODO: Execute
+        self.execute(op);
     }
 
     pub fn tick_timers(&mut self) {
@@ -90,6 +89,55 @@ impl Emulator {
         }
     }
 
+    fn addi(&mut self, op: u16, register: u16) {
+        let r1 = register as usize;
+        let value = (op & 0xFF) as u8;
+        self.v_reg[r1] = self.v_reg[r1].wrapping_add(value);
+    }
+
+    fn and(&mut self, register_1: u16, register_2: u16) {
+        let r1 = register_1 as usize;
+        let r2 = register_2 as usize;
+        self.v_reg[r1] &= self.v_reg[r2];
+    }
+
+    // CALL: call subroutine
+    fn call(&mut self, op: u16) {
+        let address = op & 0xFFF;
+        self.push(self.pc);
+        self.pc = address;
+    }
+
+    fn clean(&mut self) {
+        self.screen = [false; SCREEN_HEIGHT * SCREEN_WIDTH];
+    }
+
+    fn execute(&mut self, op: u16) {
+        let digit_1 = (op & 0xF000) >> 12;
+        let digit_2 = (op & 0x0F00) >> 8;
+        let digit_3 = (op & 0x00F0) >> 4;
+        let digit_4 = op & 0x000F;
+
+        match (digit_1, digit_2, digit_3, digit_4) {
+            (0, 0, 0, 0) => return,                      // NOP
+            (0, 0, 0xE, 0) => self.clean(),              // CLEAR SCREEN
+            (0, 0, 0xE, 0xE) => self.ret(),              // RET
+            (1, _, _, _) => self.jmp(op),                // JMP
+            (2, _, _, _) => self.call(op),               // CALL
+            (3, _, _, _) => self.seq(op, digit_2),       // SEQ
+            (4, _, _, _) => self.snq(op, digit_2),       // SNQ
+            (5, _, _, 0) => self.seqr(digit_2, digit_3), // SEQR
+            (6, _, _, _) => self.set(op, digit_2),       // SET
+            (7, _, _, _) => self.addi(op, digit_2),      // ADDI
+            (8, _, _, 0) => self.setr(digit_2, digit_3), // SETR
+            (8, _, _, 1) => self.or(digit_2, digit_3),   // OR
+            (8, _, _, 2) => self.and(digit_2, digit_3),  // AND
+            (8, _, _, 3) => self.xor(digit_2, digit_3),  // XOR
+
+            (_, _, _, _) => unimplemented!("Unimplemented op code: { }", op),
+        }
+    }
+
     // Big Endian words; opcodes has two bytes length
     fn fetch(&mut self) -> u16 {
         let higher_byte = self.ram[self.pc as usize] as u16;
@@ -97,6 +145,18 @@ impl Emulator {
         let op = (higher_byte << 8) | lower_byte; // bitwise concatenation to gen the opcode
         self.pc += 2;
         op
+    }
+
+    // JMP: jump to address encoded in op code
+    fn jmp(&mut self, op: u16) {
+        let address = op & 0xFFF;
+        self.pc = address;
+    }
+
+    fn or(&mut self, register_1: u16, register_2: u16) {
+        let r1 = register_1 as usize;
+        let r2 = register_2 as usize;
+        self.v_reg[r1] |= self.v_reg[r2];
     }
 
     fn push(&mut self, val: u16) {
@@ -107,5 +167,56 @@ impl Emulator {
     fn pop(&mut self) -> u16 {
         self.sp -= 1;
         self.stack[self.sp as usize]
+    }
+
+    // RET: Return from Subroutine
+    fn ret(&mut self) {
+        let ret_address = self.pop();
+        self.pc = ret_address;
+    }
+
+    // SEQ: skip if register equal to value
+    fn seq(&mut self, op: u16, register: u16) {
+        let r1 = register as usize;
+        let value = (op & 0xFFF) as u8;
+        if self.v_reg[r1] == value {
+            self.pc += 2;
+        }
+    }
+
+    fn set(&mut self, op: u16, register: u16) {
+        let r1 = register as usize;
+        let value = (op & 0xFF) as u8;
+        self.v_reg[r1] = value;
+    }
+
+    fn setr(&mut self, register_1: u16, register_2: u16) {
+        let r1 = register_1 as usize;
+        let r2 = register_2 as usize;
+        self.v_reg[r1] = self.v_reg[r2];
+    }
+
+    // SEQE: skip if register_1 equal to register_2
+    fn seqr(&mut self, register_1: u16, register_2: u16) {
+        let r1 = register_1 as usize;
+        let r2 = register_2 as usize;
+        if self.v_reg[r1] == self.v_reg[r2] {
+            self.pc += 2;
+        }
+    }
+
+    // SNQ: skip if register not equal to value
+    fn snq(&mut self, op: u16, register: u16) {
+        let r1 = register as usize;
+        let value = (op & 0xFFF) as u8;
+        if self.v_reg[r1] != value {
+            self.pc += 2;
+        }
+    }
+
+    fn xor(&mut self, register_1: u16, register_2: u16) {
+        let r1 = register_1 as usize;
+        let r2 = register_2 as usize;
+        self.v_reg[r1] ^= self.v_reg[r2];
     }
 }
