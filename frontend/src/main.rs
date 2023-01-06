2e7b213ade7b19b3c5aded25613921457c4f85d3
paulopacitti <ppacitti@outlook.com>
2023-01-06 09:24:38 -0300

    Init frontend development with SDL2

--- frontend/src/main.rs
+++ frontend/src/main.rs
@@ -1,3 +1,40 @@
+use core::*;
+use sdl2::event::Event;
+use std::env;
+
+const SCALE: u32 = 15; // 15x native scale
+const WINDOW_HEIGHT: u32 = (SCREEN_HEIGHT as u32) * SCALE;
+const WINDOW_WIDTH: u32 = (SCREEN_WIDTH as u32) * SCALE;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<_> = env::args().collect();
+    if args.len() != 2 {
+        println!("Usage: cargon run <path-to-game>");
+        return;
+    }
+
+    let sdl = sdl2::init().unwrap();
+    let video_subsystem = sdl.video().unwrap();
+    let window = video_subsystem
+        .window("chip-r", WINDOW_WIDTH, WINDOW_HEIGHT)
+        .position_centered()
+        .opengl()
+        .build()
+        .unwrap();
+
+    let mut canvas = window.into_canvas().present_vsync().build().unwrap();
+    canvas.clear();
+    canvas.present();
+
+    let mut event_pump = sdl.event_pump().unwrap();
+    'gameloop: loop {
+        for event in event_pump.poll_iter() {
+            match event {
+                Event::Quit { .. } => {
+                    break 'gameloop;
+                }
+                _ => (),
+            }
+        }
+    }
 }
